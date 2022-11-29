89246c66f1906e35f64f318ec58347186a86e9c3
Ansar Memon <ansar.memon@hotmail.com>
2022-11-28 20:17:04 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Ansar Memon - Software Engineer @ Microsoft 
+
+A pragmatic Frontend Engineer, with a strong experience working with Javascript, Typescript, React, React-Native, AngularJS, NodeJS and state management tools like Redux, React Query, Zustand and Redux-sagas. For running tests, its usually Jest (unit testing) and Cypress (e2e testing). I also work extensively with Docker containers, Travis and Kubernetes for CI / CD. 
+
+I am also enthusiastic about cloud computing and blockchain technologies. I am known for my enthusiasm to learn and apply the latest tech stack where it makes sense. Within the Frontend domain, my main interests lie in architecture and design patterns for building scalable applications.
+
+Other languages/frameworks that I have worked with - GraphQL, Golang, PHP, and Java with Spring boot framework
+
+Design Tools - Figma | Adobe Illustrator | Photoshop | Indesign | Sketch | After Effects.
+`);
